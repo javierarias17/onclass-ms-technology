5825bbf812b027383e461437a2c4f91c023ece7b
Javier Arias <parias@heinsohn.com>
2026-07-24 11:09:20 -0500

    Se optimiza la consulta de validación de IDs y se agrega manejo de cuerpo de solicitud vacío para el registro de tecnología

--- infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/co/com/pragma/r2dbc/TechnologyReactiveRepository.java
+++ infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/co/com/pragma/r2dbc/TechnologyReactiveRepository.java
@@ -1,13 +1,21 @@
 package co.com.pragma.r2dbc;
 
 import co.com.pragma.r2dbc.entity.TechnologyEntity;
+import org.springframework.data.r2dbc.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.repository.query.ReactiveQueryByExampleExecutor;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.util.List;
 
 public interface TechnologyReactiveRepository extends
         ReactiveCrudRepository<TechnologyEntity, Long>,
         ReactiveQueryByExampleExecutor<TechnologyEntity> {
 
     Mono<Boolean> existsByName(String name);
+
+    @Query("SELECT id FROM technologies WHERE id IN (:technologyIds)")
+    Flux<Long> findExistingIds(@Param("technologyIds") List<Long> technologyIds);
 }
